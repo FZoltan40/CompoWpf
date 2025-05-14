29136a6cbfdc402a0d72a87eb489591a56517893
FZoltan40 <farkasz@kkszki.hu>
2025-05-14 12:01:55 +0200

    Register is ready to use with hashed password.

--- ComputerWpf/Page2.xaml.cs
+++ ComputerWpf/Page2.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ComputerWpf
 {
@@ -14,13 +19,11 @@ namespace ComputerWpf
             _mainWindow = mainWindow;
         }
 
-        private void registerButton1_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-
-        }
+        private const string ConnectionString = "Server=localhost;Database=computer;Uid=root; Password=;SslMode=None";
 
         private void registerButton1_Click_1(object sender, System.Windows.RoutedEventArgs e)
         {
+            MessageBox.Show(RegisterUser(usernameTextBox.Text, passwordBox.Password, emailTextBox.Text));
 
         }
 
@@ -28,5 +31,62 @@ namespace ComputerWpf
         {
             _mainWindow.MainFrame.Navigate(new Page1(_mainWindow));
         }
+
+        private string GenerateSalt()
+        {
+            byte[] salt = new byte[16];
+
+            using (var rnd = RandomNumberGenerator.Create())
+            {
+                rnd.GetBytes(salt);
+            }
+
+            return Convert.ToBase64String(salt);
+        }
+
+        private string ComputeHmacSha256(string password, string salt)
+        {
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(salt)))
+            {
+                byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private string RegisterUser(string userName, string userPassword, string userEmail)
+        {
+            try
+            {
+                string salt = GenerateSalt();
+                string hashedPassword = ComputeHmacSha256(userPassword, salt);
+
+                using (var connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO `users`(`UserName`, `Email`, `PasswordHash`, `Salt`) VALUES (@userName,@userEmail,@paswordHash,@salt)";
+
+                    using (var cmd = new MySqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@userName", userName);
+                        cmd.Parameters.AddWithValue("@userEmail", userEmail);
+                        cmd.Parameters.AddWithValue("@paswordHash", hashedPassword);
+                        cmd.Parameters.AddWithValue("@salt", salt);
+
+                        cmd.ExecuteNonQuery();
+
+                    }
+
+                    connection.Close();
+                }
+                return "Sikeres regisztráció!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
     }
 }
+
